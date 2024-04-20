6634e77e5a6a81b51d26b32bfb3ada3909e8d17b
Jonas <publik.froeller.jonas@gmx.at>
2024-04-20 17:19:20 +0200

    deps and code docs

--- main.ts
+++ main.ts
@@ -5,16 +5,17 @@ import { upgradeWebSocket, jwtSign } from 'https://deno.land/x/hono@v4.2.5/helpe
 import { logger, poweredBy, jwt } from 'https://deno.land/x/hono@v4.2.5/middleware.ts'
 import { home } from "./home.tsx"
 import {
-  /* getSignedCookie, */
   setSignedCookie,
   deleteCookie,
 } from 'https://deno.land/x/hono@v4.2.5/helper.ts'
 import { cors } from 'https://deno.land/x/hono@v4.2.5/middleware.ts'
 import { ChatRoom } from "./Chatroom.ts";
-// import { Server as WebsocketServer } from "https://deno.land/x/socket_io@0.2.0/mod.ts";
-// import { serve as serveHttpServer } from "https://deno.land/std@0.150.0/http/server.ts";
-// INFO: Unable to attach a second server to deno serve => manual room implementation needed.
-// AND(https://github.com/socketio/socket.io/tree/main?tab=readme-ov-file#room-support): Socket.IO is not a WebSocket implementation. Although Socket.IO indeed uses WebSocket as a transport when possible, it adds some metadata to each packet: the packet type, the namespace and the ack id when a message acknowledgement is needed. That is why a WebSocket client will not be able to successfully connect to a Socket.IO server, and a Socket.IO client will not be able to connect to a WebSocket server (like ws://echo.websocket.org) either. Please see the protocol specification here.
+// Why not socket.io? (https://github.com/socketio/socket.io/tree/main?tab=readme-ov-file#room-support):
+// Socket.IO is not a WebSocket implementation. 
+// Although Socket.IO indeed uses WebSocket as a transport when possible, it adds some metadata to each packet: 
+// the packet type, the namespace and the ack id when a message acknowledgement is needed. 
+// That is why a WebSocket client will not be able to successfully connect to a Socket.IO server, 
+// and a Socket.IO client will not be able to connect to a WebSocket server (like ws://echo.websocket.org) either. Please see the protocol specification here.
 
 /* CONFIGURATION */
 const app = new Hono()
@@ -26,12 +27,16 @@ const JWT_OPTIONS = {
 
 /* CHAT */
 /* CHAT GUARD */
+/**
+ * Should work with headers like Authorization: Bearer <token>
+ * and cookies: propromo-chat-login: Bearer <token>
+ */
 app.use('/chat/*', (c, next) => {
   const jwtMiddleware = jwt({
     secret: JWT_OPTIONS.secret,
     cookie: 'propromo-chat-login',
     alg: JWT_OPTIONS.alg
-  }) // jwtVerify() && jwtDecode() // should work with headers like Authorization: Bearer <token> and cookies: await getSignedCookie(c, JWT_SECRET, 'propromo-chat-login')
+  }) // jwtVerify() && jwtDecode()
   return jwtMiddleware(c, next)
 })
 
@@ -75,14 +80,14 @@ app.use('*', logger(), poweredBy(), cors({
 }))
 app.route("", home)
 
-/* LOGIN */
+/* AUTHENTICATION ENDPOINT */
 app.post('/login', (c) => {
   return c.req.parseBody().then(async (body) => {
     const token = await jwtSign({
       monitor_id: body.monitor_id // email and password is only needed for initial validation, after that the token is the validation
     }, JWT_OPTIONS.secret, JWT_OPTIONS.alg);
 
-    deleteCookie(c, 'propromo-chat-login')
+    deleteCookie(c, 'propromo-chat-login') // await getSignedCookie(c, JWT_SECRET, 'propromo-chat-login')
     await setSignedCookie(c, 'propromo-chat-login', "Bearer " + token, JWT_SECRET, {
       domain: '127.0.0.1:6969',
       secure: true,
