52fefef4f324611187fe31d8342a9094569deb21
Jonas <publik.froeller.jonas@gmx.at>
2024-04-20 14:55:15 +0200

    fixed websocket chat endpoint

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
 
 import { Hono } from 'https://deno.land/x/hono@v4.2.5/mod.ts'
 import { upgradeWebSocket, jwtSign } from 'https://deno.land/x/hono@v4.2.5/helper.ts'
-import { logger, poweredBy, jwt } from 'https://deno.land/x/hono@v4.2.5/middleware.ts' // https://hono.dev/middleware/builtin/jwt
+import { logger, poweredBy, jwt } from 'https://deno.land/x/hono@v4.2.5/middleware.ts'
 import { home } from "./home.tsx"
 import {
   getSignedCookie,
@@ -11,36 +11,34 @@ import {
 } from 'https://deno.land/x/hono@v4.2.5/helper.ts'
 import { cors } from 'https://deno.land/x/hono@v4.2.5/middleware.ts'
 
-const JWT_SECRET = "testing"
+const JWT_SECRET = "testing" // c.env.JWT_SECRET
 const JWT_OPTIONS = {
-  secret: JWT_SECRET, // c.env.JWT_SECRET,
+  secret: JWT_SECRET,
   alg: "HS256" as "HS256" | "HS384" | "HS512" | "RS256" | "RS384" | "RS512" | "PS256" | "PS384" | "PS512" | "ES256" | "ES384" | "ES512" | "EdDSA" | undefined
 }
 
-export const app = new Hono().route("", home)
-app.use('*', logger(), poweredBy())
-app.use(
+export const app = new Hono()/* .route("", home) */
+/* app.use('*', logger(), poweredBy()) */
+/* app.use(
   '*',
   cors()
-)
+) */
 
 /* LOGIN */
 app.post('/login', (c) => {
   return c.req.parseBody().then(async (body) => {
-    const token = await jwtSign(body, JWT_OPTIONS.secret, JWT_OPTIONS.alg);
+    const token = await jwtSign({
+      monitor_id: body.monitor_id // email and password is only needed for initial validation, after that the token is the validation
+    }, JWT_OPTIONS.secret, JWT_OPTIONS.alg);
 
-    // `getSignedCookie` will return `false` for a specified cookie if the signature was tampered with or is invalid
-    /* const allSignedCookies = await getSignedCookie(c, JWT_SECRET)
-    const fortuneCookie = await getSignedCookie(c, JWT_SECRET, 'fortune_cookie') */
-
+    deleteCookie(c, 'propromo-chat-login')
     await setSignedCookie(c, 'propromo-chat-login', token, JWT_SECRET, {
+      domain: '127.0.0.1:6969',
       secure: true,
-      sameSite: 'Strict',
+      sameSite: 'Lax',
       httpOnly: true,
       path: `/chat/${body.monitor_id}`
     })
-
-    // deleteCookie(c, 'propromo-chat-login')
 
     return c.json({
       body,
@@ -56,7 +54,7 @@ app.use('/chat/*', (c, next) => {
     secret: JWT_OPTIONS.secret,
     cookie: 'propromo-chat-login',
     alg: JWT_OPTIONS.alg
-  }) // jwtVerify() && jwtDecode()
+  }) // jwtVerify() && jwtDecode() // should work with headers like Authorization: Bearer <token> and cookies: await getSignedCookie(c, JWT_SECRET, 'propromo-chat-login')
   return jwtMiddleware(c, next)
 })
 
