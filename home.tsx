9dfedb68f921a1057c8a24e0077c003d1d7d2a15
Jonas <publik.froeller.jonas@gmx.at>
2024-04-20 02:20:12 +0200

    init

---
+++ home.tsx
@@ -0,0 +1,63 @@
+/** @jsx jsx */
+import { Hono } from 'https://deno.land/x/hono@v4.2.5/mod.ts'
+import { logger, poweredBy, jsx, serveStatic } from 'https://deno.land/x/hono@v4.2.5/middleware.ts'
+import { html } from "https://deno.land/x/hono@v4.2.5/helper/html/index.ts";
+
+type Props = {
+    title: string
+    children?: any
+}
+
+const Layout = (props: Props) => html`<!DOCTYPE html>
+  <html>
+    <link
+      type="image/png"
+      rel="icon"
+      href="/favicon.png"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+    />
+    <head>
+      <title>${props.title}</title>
+    </head>
+    <body>
+      ${props.children}
+    </body>
+  </html>`
+
+export const home = new Hono()
+
+home.use('*', logger(), poweredBy())
+home.all('/favicon.png', serveStatic({ path: './public/favicon.png' }))
+
+const LogInForm = () => {
+  return (
+    <form action="/login" method="post">
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" name="email" />
+      <br />
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password" name="password" />
+      <br />
+      <label htmlFor="monitor_id">Monitor ID:</label>
+      <input type="text" id="monitor_id" name="monitor_id" />
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+home.get('', (c) => {
+    return c.html(
+      <Layout title="Login">
+        <header class="container">
+        <h1>Login with your Propromo Account and a valid monitor-id.</h1>
+        </header>
+        <main class="container">
+          <LogInForm />
+        </main>
+      </Layout>
+    )
+  })
---
+++ main.ts
@@ -0,0 +1,76 @@
+import { Hono } from 'https://deno.land/x/hono@v4.2.5/mod.ts'
+import { upgradeWebSocket, jwtSign } from 'https://deno.land/x/hono@v4.2.5/helper.ts'
+import { logger, poweredBy, jwt } from 'https://deno.land/x/hono@v4.2.5/middleware.ts' // https://hono.dev/middleware/builtin/jwt
+import { home } from "./home.tsx"
+import {
+  getSignedCookie,
+  setSignedCookie,
+  deleteCookie,
+} from 'https://deno.land/x/hono@v4.2.5/helper.ts'
+
+const JWT_SECRET = "testing"
+const JWT_OPTIONS = {
+  secret: JWT_SECRET, // c.env.JWT_SECRET,
+  alg: "HS256" as "HS256" | "HS384" | "HS512" | "RS256" | "RS384" | "RS512" | "PS256" | "PS384" | "PS512" | "ES256" | "ES384" | "ES512" | "EdDSA" | undefined
+}
+
+export const app = new Hono().route("", home)
+app.use('*', logger(), poweredBy())
+
+/* LOGIN */
+app.post('/login', (c) => {
+  return c.req.parseBody().then(async (body) => {
+    const token = await jwtSign(body, JWT_OPTIONS.secret, JWT_OPTIONS.alg);
+
+    // `getSignedCookie` will return `false` for a specified cookie if the signature was tampered with or is invalid
+    /* const allSignedCookies = await getSignedCookie(c, JWT_SECRET)
+    const fortuneCookie = await getSignedCookie(c, JWT_SECRET, 'fortune_cookie') */
+
+    await setSignedCookie(c, 'propromo-chat-login', token, JWT_SECRET, {
+      secure: true,
+      sameSite: 'Strict',
+      httpOnly: true,
+      path: `/chat/${body.monitor_id}`
+    })
+
+    // deleteCookie(c, 'propromo-chat-login')
+
+    return c.json({
+      body,
+      token
+    })
+  })
+})
+
+/* CHAT */
+/* CHAT GUARD */
+app.use('/chat/*', (c, next) => {
+  const jwtMiddleware = jwt({
+    secret: JWT_OPTIONS.secret,
+    cookie: 'propromo-chat-login',
+    alg: JWT_OPTIONS.alg
+  }) // jwtVerify() && jwtDecode()
+  return jwtMiddleware(c, next)
+})
+
+/* CHAT CONNECTION INFO (GET PAYLOAD OF JWT) */
+app.get('/chat/info/payload', (c) => {
+  const payload = c.get('jwtPayload')
+  return c.json(payload)
+})
+
+/* CHAT ROOM */
+app.get('/chat/:monitor_id', upgradeWebSocket((c) => {
+  const monitor_id = c.req.param('monitor_id')
+
+  return {
+    onMessage(event, ws) {
+      ws.send(event.data.toString() + " in " + monitor_id)
+    },
+    onClose: () => {
+      console.log('Connection closed')
+    },
+  }
+}))
+
+Deno.serve({ port: 6969 }, app.fetch)
