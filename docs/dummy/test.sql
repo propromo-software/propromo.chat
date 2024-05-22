2199631a95e6035c11b3aad4dad12b548a16fd4f
jonasfroeller <publik.froeller.jonas@gmx.at>
2024-05-22 22:09:25 +0200

    docs(dummy): test.sql

--- docs/dummy/test.sql
+++ docs/dummy/test.sql
@@ -143,15 +143,11 @@ CREATE TABLE repositories (
 INSERT INTO users (name, email, password, auth_type)
 VALUES ('John Doe', 'j.froe@gmx.at', 'password', 'local');
 
-SELECT id INTO user_id FROM users WHERE email = 'j.froe@gmx.at';
-
-INSERT INTO monitors (monitor_hash, project_identification)
-VALUES ('w32tgse', 12345);
-
-SELECT id INTO monitor_id FROM monitors WHERE monitor_hash = 'w32tgse';
+INSERT INTO monitors (type, monitor_hash, project_identification)
+VALUES ('ORGANIZATION', 'w32tgse', 12345);
 
 INSERT INTO monitor_user (user_id, monitor_id)
-VALUES (user_id, monitor_id);
+VALUES ((SELECT id FROM users WHERE email = 'j.froe@gmx.at'), (SELECT id FROM monitors WHERE monitor_hash = 'w32tgse'));
 
 SELECT user_id FROM monitor_user mu
       LEFT JOIN users u ON mu.user_id = u.id
@@ -163,10 +159,8 @@ SELECT user_id FROM monitor_user mu
 INSERT INTO users (name, email, password, auth_type)
 VALUES ('New User', 'newuser@example.com', 'newpassword', 'local');
 
-SELECT id INTO new_user_id FROM users WHERE email = 'newuser@example.com';
-
 INSERT INTO monitor_user (user_id, monitor_id)
-VALUES (new_user_id, monitor_id);
+VALUES ((SELECT id FROM users WHERE email = 'newuser@example.com'), (SELECT id FROM monitors WHERE monitor_hash = 'w32tgse'));
 
 SELECT user_id FROM monitor_user mu
       LEFT JOIN users u ON mu.user_id = u.id
