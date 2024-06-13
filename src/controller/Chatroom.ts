89c9c31d6a27e70a1cdeb3b5704b3cf6d42dfa00
Jonas <publik.froeller.jonas@gmx.at>
2024-06-13 21:59:53 +0200

    fix(ping|pong): do not store pings

--- src/controller/Chatroom.ts
+++ src/controller/Chatroom.ts
@@ -74,6 +74,12 @@ export class ChatRoom {
 
     async onMessage(event: MessageEvent, sender?: { email: string | undefined, ws: WSContext }): Promise<void> {
         const message = event.data;
+
+        if (message === 'ping') {
+            console.log("pong");
+            return;
+        }
+
         const messageData = await this.createMessage(sender?.email ?? 'unknown@unknown.tld', message);
         const broadcastMessage = JSON.stringify(messageData);
         this.broadcast(broadcastMessage/* , sender */);
